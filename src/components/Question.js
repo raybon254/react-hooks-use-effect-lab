bfcc6e10238b6837bb867724231a5a91f6216345
Brian <musyimib449@gmail.com>
2025-04-14 17:31:49 +0300

    hooks use effect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import { set } from "date-fns";
+import React, { useEffect,useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      setTimeRemaining((prev) => {
+        if(prev === 0){
+          onAnswered(false)
+          return 10;
+         }
+        return prev - 1;
+      })
+    }, 1000);
+    return function intervaltime(){
+      clearTimeout(interval)
+     }
+  },[onAnswered])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
